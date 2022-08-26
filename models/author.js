be29ffe1ac5c26e7a3591a7a3fcca4c72c20bf56
boycany <yurayurateiko@gmail.com>
2022-08-26 13:50:43 +0800

    book_detail, author_list, genre_list, genre_detail done

--- models/author.js
+++ models/author.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const { DateTime } = require("luxon")
+
 const AuthorSchema = new Schema({
   first_name: {
     type: String,
@@ -23,5 +25,14 @@ AuthorSchema.virtual('name').get(function(){
 AuthorSchema.virtual('url').get(function(){
   return "/catalog/author/" + this._id;
 });
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function(){
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : ''
+})
+
+AuthorSchema.virtual('date_of_death_formatted').get(function(){
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : ''
+})
+
 
 module.exports = mongoose.model("Author", AuthorSchema);
