334496bcd61e6c531db2caa755ad33466081d02c
boycany <yurayurateiko@gmail.com>
2022-08-26 15:18:47 +0800

    author_detail, bookinstance_detail done

--- models/author.js
+++ models/author.js
@@ -26,13 +26,11 @@ AuthorSchema.virtual('url').get(function(){
   return "/catalog/author/" + this._id;
 });
 
-AuthorSchema.virtual('date_of_birth_formatted').get(function(){
-  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : ''
-})
+AuthorSchema.virtual('lifespan').get(function(){
+  const formatted_birth = this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : ''
+  const formatted_death = this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : ''
 
-AuthorSchema.virtual('date_of_death_formatted').get(function(){
-  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : ''
+  return formatted_birth + " - " + formatted_death
 })
-
 
 module.exports = mongoose.model("Author", AuthorSchema);
